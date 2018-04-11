08a2ca0b1fa7c446cb834ebd4e11fb83f5719619
rosa <rozumaeta@ucm.es>
2018-04-11 23:10:13 +0200

    feat(idea-models): Se incluye la entidad básica y los modelos de BBDD

---
+++ models/usuario.php
@@ -0,0 +1,62 @@
+<?php
+include 'models/entidadBase.php';
+class Usuario extends EntidadBase {
+    private $id_correo;
+	private $password;
+    private $nombre;
+    private $imagen;
+    private $admin;
+    
+     
+    public function __construct(){ 
+		$this->table = "usuario";
+        $class = "Usuario";
+        parent::__construct($this->table, $class);
+    }
+     
+    public function getIdCorreo() {
+        return $this->idCorreo;
+    }
+ 
+    public function setIdCorreo($idCorreo) {
+        $this->id_correo = $idCorreo;
+    }
+	
+	public function getPassword() {
+        return $this->password;
+    }
+ 
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+     
+    public function getNombre() {
+        return $this->nombre;
+    }
+ 
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+  
+    public function getImagen() {
+        return $this->imagen;
+    }
+ 
+    public function setImagen($imagen) {
+        $this->imagen = $imagen;
+    } 
+	
+	public function setUser($user){
+		//echo "Guardando usuario..";
+        $query="INSERT INTO usuario (id_correo,password,nombre,imagen,admin)
+                VALUES('".$user->id_correo."',
+                       '".$user->password."',
+                       '".$user->nombre."',
+                       '".$user->imagen."',
+                       '".$user->admin."');";
+        if($this->db()->query($query) == TRUE){
+			echo "Usuario creado correctamente";
+		}
+    }
+}
+?>
