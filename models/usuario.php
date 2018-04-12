25a65d3149886dd53e1a3c656890ec771cde98cf
Miguel Arriba <marriba@ucm.es>
2018-04-12 22:55:22 +0200

    feat(usuario-models) Login con BD

--- models/usuario.php
+++ models/usuario.php
@@ -1,51 +1,70 @@
 <?php
-include 'models/entidadBase.php';
+include 'entidadBase.php';
 class Usuario extends EntidadBase {
     private $id_correo;
-	private $password;
+	  private $password;
     private $nombre;
     private $imagen;
     private $admin;
-    
-     
-    public function __construct(){ 
+
+
+    public function __construct(){
 		$this->table = "usuario";
         $class = "Usuario";
         parent::__construct($this->table, $class);
     }
-     
+
     public function getIdCorreo() {
         return $this->idCorreo;
     }
- 
+
     public function setIdCorreo($idCorreo) {
         $this->id_correo = $idCorreo;
     }
-	
+
 	public function getPassword() {
         return $this->password;
     }
- 
+
     public function setPassword($password) {
         $this->password = $password;
     }
-     
+
     public function getNombre() {
         return $this->nombre;
     }
- 
+
     public function setNombre($nombre) {
         $this->nombre = $nombre;
     }
-  
+
     public function getImagen() {
         return $this->imagen;
     }
- 
+
     public function setImagen($imagen) {
         $this->imagen = $imagen;
-    } 
-	
+    }
+
+	public function getUser($user, $pass){
+    echo "</br>";
+    //Hace la consulta
+    $sql = "SELECT id_correo,password FROM usuario WHERE id_correo = '$user'";
+    //Guarda el resultado de la consulta en la variable.
+    $consulta = mysqli_query($this->db(), $sql);
+    //Comprueba si la consulta es vacia
+    if (mysqli_num_rows($consulta) == 0) return false;
+    //Lee la primera fila
+    $fila = mysqli_fetch_array($consulta);
+
+
+    //Comprueba los resultados
+    if($fila['id_correo'] == $user)
+      if($fila['password'] == $pass) return true;
+    return false;
+
+  }
+
 	public function setUser($user){
 		//echo "Guardando usuario..";
         $query="INSERT INTO usuario (id_correo,password,nombre,imagen,admin)
