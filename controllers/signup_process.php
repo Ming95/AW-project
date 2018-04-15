4dda6387e45a13696e3685cf1d66ce6b24c843dc
Miguel Arriba <marriba@ucm.es>
2018-04-15 13:13:54 +0200

    fix(singup - controllers)

--- controllers/signup_process.php
+++ controllers/signup_process.php
@@ -1,10 +1,11 @@
 <?php
+	session_start();
 	$username = htmlspecialchars(trim(strip_tags($_REQUEST["uname"])));
-	$userlastname = htmlspecialchars(trim(strip_tags($_REQUEST["ulast"])));
-    $password = htmlspecialchars(trim(strip_tags($_REQUEST["psw"])));
+  $password = htmlspecialchars(trim(strip_tags($_REQUEST["psw"])));
 	$password2 = htmlspecialchars(trim(strip_tags($_REQUEST["psw2"])));
 	$mail = htmlspecialchars(trim(strip_tags($_REQUEST["mail"])));
-	if($password == password2){
+	echo "Work in progress";
+	if($password == $password2){
 		//public metododesignup($username, $userlastname, $password, $mail);
 	}
 ?>
--- views/signup.php
+++ views/signup.php
@@ -1,21 +1,18 @@
 
 <meta charset="utf-8">
-<link rel="stylesheet" type="text/css" href="css/head.css" />
-<link rel="stylesheet" type="text/css" href="css/foot_page.css" />
+<link rel="stylesheet" type="text/css" href="/css/head.css" />
+<link rel="stylesheet" type="text/css" href="/css/foot_page.css" />
 <title>Login</title>
 
-<?php include 'views/layout/head.php';?>
+<?php include './layout/head.php';?>
 <div id="contenedor">
 	<?php
-			echo"<form action="."signup_process.php"." method="."post".">";
+			echo"<form action="."/controllers/signup_process.php"." method="."post".">";
 
-				echo"<label for="."uname"."><b>name</b></label>";
-				echo"<input type="."text"." placeholder="."Enter User name"." name="."uname"." required>";
+				echo"<label for="."uname"."><b>Username</b></label>";
+				echo"<input type="."text"." placeholder="."Enter Username"." name="."uname"." required>";
 				echo"</br>";
-				echo"<label for="."ulast"."><b>Lastname</b></label>";
-				echo"<input type="."text"." placeholder="."Enter Last name"." name="."ulast"." required>";
-				echo"</br>";
-				
+
 				echo"<label for="."mail"."><b>Email</b></label>";
 				echo"<input type="."email"." placeholder="."Enter Email"." name="."mail"." required>";
 				echo"</br>";
@@ -23,15 +20,14 @@
 				echo"<label for="."psw"."><b>Password</b></label>";
 				echo"<input type="."password"." placeholder="."Enter Password"." name="."psw"." required>";
 				echo"</br>";
-				echo"<label for="."psw2"."><b>Password2</b></label>";
+				echo"<label for="."psw2"."><b>Repeat password</b></label>";
 				echo"<input type="."password"." placeholder="."Enter Password again"." name="."psw2"." required>";
 				echo"</br>";
-				
+
 				echo"<button type="."submit".">Signup</button>";
 				echo"</br>";
 			echo"</form>";
 	?>
 </div>
 <hr>
-<?php include 'views/layout/foot_page.php';?>
-
+<?php include './layout/foot_page.php';?>
