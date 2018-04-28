183367194210802b5edf263f21845f71b27df17b
rosa <rozumaeta@ucm.es>
2018-04-28 18:51:09 +0200

    feat(idea) Se incluye ventana de error para elevar excepciones al usuario - errorpage.php

---
+++ errorpage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<head>
+<link rel="stylesheet" type="text/css" href="/css/styleforms.css" />
+<script type="text/javascript" src="../js/utilidea.js"></script>
+</head>
+<body>
+
+
+<div class="container">
+    <div class="row">
+	<?php
+		session_start();
+        echo '<label class="texto2">'.$_SESSION['error'].'</label>';
+	?>
+    </div>
+     <input class="submit" type="submit" value="Cerrar" onclick="volver()">
+</div>
+
+</body>
+</html>
