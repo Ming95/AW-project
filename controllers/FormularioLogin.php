dedf94c8a0208a939120cad914ad259038b9be69
Miguel Arriba <marriba@ucm.es>
2018-04-29 16:21:09 +0200

    feat(login) Se aplica el patron Template
    Toda la lógica pasa a estar en FormularioLogin.php

---
+++ controllers/FormularioLogin.php
@@ -0,0 +1,70 @@
+<?php
+include 'Form.php';
+include '../models/usuario.php';
+class FormularioLogin extends Form
+{
+    public function __construct() {
+        parent::__construct('formLogin');
+    }
+
+    protected function generaCamposFormulario($datos)
+    {
+
+        $html = <<<EOF
+        			<legend>Iniciar Sesion</legend>
+        				<div class="campos-formulario">
+        				<h4>Email</h4> <input class ="input-box" type="text" name="mail" placeholder="Introduce tu e-mail" required>
+
+        				<h4>Contraseña</h4>	<input class ="input-box" type="password" name="psw" placeholder="Introduce una contraseña entre 8 y 16 caracteres"
+        											pattern="^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$" required>
+        				<p>La contraseña requiere al menos: un dígito, una minúscula, una mayúscula y un caracter no alfanumérico.</p>
+
+        				</div>
+        				<div class="submit-formulario">
+        					<input type="button" value="REGISTRARSE" class ="boton-formulario2" onclick="location.href='/views/signup.php'">
+        					<input type="submit" value="INICIAR SESIÓN" class ="boton-formulario">
+        				</div>
+EOF;
+        return $html;
+    }
+
+    protected function procesaFormulario($datos)
+    {
+      $result = array();
+      $username = htmlspecialchars(trim(strip_tags($_REQUEST["mail"])));
+      $password = htmlspecialchars(trim(strip_tags($_REQUEST["psw"])));
+
+      if(!isset($_SESSION['intentos'])){
+        $_SESSION['intentos'] = 0;
+        $_SESSION["logged"]=false;
+      }
+      if(!isset($_SESSION['logged'])){
+        $_SESSION["logged"]=false;
+      }
+      try{
+        //Se crea el objeto usuario y se abre conexión
+        $user = new Usuario();
+        //Consultamos si existe el usuario
+        $consulta = $user->getBy("id_correo",$username);
+        //Cerramos la conexión tras realizar la consulta
+        $user->closeConnection();
+        //Generamos el hash de la password en claro
+        $hash=SHA1($password);
+        //Comparamos el nuevo hash con el existente en BBDD
+        if($consulta!=null and hash_equals($hash,$consulta[0]['password'])){
+          $_SESSION["logged"]	= true;
+          $_SESSION['login'] = $consulta[0]['nombre'];
+          $_SESSION['mail'] = $consulta[0]['id_correo'];
+          $_SESSION['intentos'] = 0;
+          $result = '../index.php';
+        }
+        else $result[] = "Email o contraseña incorrecta";
+
+      }catch (Exception $e) {
+        error_log("MySQL: Code: ".$e->getCode(). " Desc: " .$e->getMessage() ,0);
+        $_SESSION['data_error']=$e->getMessage();
+        $result = '../errorpage.php';
+      }
+      return $result;
+    }
+}
--- views/login.php
+++ views/login.php
@@ -1,34 +1,23 @@
-<meta charset="utf-8">
-<link rel="stylesheet" type="text/css" href="/css/head.css" />
-<link rel="stylesheet" type="text/css" href="/css/foot_page.css" />
-<link rel="stylesheet" type="text/css" href="/css/formulario.css" />
-<title>Login</title>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="/css/head.css" />
+	<link rel="stylesheet" type="text/css" href="/css/foot_page.css" />
+	<link rel="stylesheet" type="text/css" href="/css/formulario.css" />
+	<title>Login</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Login</title>
+</head>
 
-<?php include 'layout/head.php';?>
-<div class="contenedor">
-		<form action="/controllers/login_process.php" method="post" class="formulario">
-			<legend>Iniciar Sesion</legend>
-				<div class="campos-formulario">
-				<h4>Email</h4> <input class ="input-box" type="text" name="mail" placeholder="Introduce tu e-mail" required>
+<body>
+	<?php
+			include 'layout/head.php';
+			include '../controllers/FormularioLogin.php';
 
-				<h4>Contraseña</h4>	<input class ="input-box" type="password" name="psw" placeholder="Introduce una contraseña entre 8 y 16 caracteres"
-											pattern="^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$" required>
-				<p>La contraseña requiere al menos: un dígito, una minúscula, una mayúscula y un caracter no alfanumérico.</p>
-				<?php
-						if(!isset($_SESSION['intentos'])or($_SESSION['intentos'] )< 3){
-								if(isset($_SESSION['intentos']))
-									echo "<p class="."error".">Usuario o contraseña incorrectos.</p>";
-						}else{
-							$_SESSION['intentos'] = 0;
-							echo"<p class="."error".">¿Has olvidado tu contraseña?</p>";
-							echo'<a href="../index.php">Recuperar contraseña.</a>';
-						}
-				?>
-				</div>
-				<div class="submit-formulario">
-					<input type="button" value="REGISTRARSE" class ="boton-formulario2" onclick="location.href='/views/signup.php'">
-					<input type="submit" value="INICIAR SESIÓN" class ="boton-formulario">
-				</div>
-			</form>
-</div>
-<?php include 'layout/foot_page.php';?>
+			$form = new FormularioLogin();
+			$form->gestiona();
+			include 'layout/foot_page.php';
+	?>
+
+</body>
+</html>
