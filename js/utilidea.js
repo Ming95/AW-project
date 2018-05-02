7ee6f9fd7c746c2ae9766981d720210e638335a5
rosa <rozumaeta@ucm.es>
2018-05-02 14:34:14 +0200

    feat(idea-js) Se incluyen funciones para generar caja de comentarios - utilidea.js

--- js/utilidea.js
+++ js/utilidea.js
@@ -37,24 +37,56 @@ function showContent() {
 			document.getElementById("lista3").className = 'nactive';
 		}
 		
-		function myfunction2(e){
+		function insertaComentario(){
+			var parent = document.getElementById("div1");
+			var para = document.createElement("p");
+			var texto = document.getElementById("subject").value;
+			var node = document.createTextNode(texto);
+			para.appendChild(node);
+			parent.appendChild(para);
+			var linea = document.createElement("hr");
+			parent.appendChild(linea);
+		}
+		
+		function mostrarComentarios(comentarios){
+			var types = JSON.parse(comentarios);
+			var parent = document.getElementById("div1");
+			for (paso = 0; paso < types.length; paso++) {
+				var para = document.createElement("p");
+				var texto= types[paso].fecha_creacion;
+				var node = document.createTextNode(texto);
+				para.appendChild(node);
+				parent.appendChild(para);
+				var para1 = document.createElement("p1");
+				var texto1= types[paso].comentario;
+				var node1 = document.createTextNode(texto1);
+				para1.appendChild(node1);
+				parent.appendChild(para1);
+				var linea = document.createElement("hr");
+				parent.appendChild(linea);
+			}
+		}
+		
+		function myf2(){
 			var elemento1 = document.getElementById("datos1");
 			elemento1.style.display = 'none';
-			
-		//	alert('holaa1');
+
 			var elemento2 = document.getElementById("datos2");
 			elemento2.style.display = 'block';
-		//	alert('holaa2');
-			
+			//mostrarComentarios();
 			var elemento3 = document.getElementById("datos3");
 			elemento3.style.display = 'none';
-		//	alert('holaa2');
-			//var elemento = document.getElementById("lista2");
 			document.getElementById("lista2").className = 'active';
 			document.getElementById("lista1").className = 'nactive';
 			document.getElementById("lista3").className = 'nactive';
-
 		}
+			
+		function myfunction2(coment){
+			mostrarComentarios(coment);
+			myf2();
+			//window.location.href = '../controllers/consultar_idea_process.php?id_idea='+id_idea+'&comentario='+1;
+		}
+
 		
 		function myfunction3(e){
 			var elemento1 = document.getElementById("datos1");
@@ -80,11 +112,13 @@ function showContent() {
 				elemento.style.display = 'block';
 			}
 		}
+		
 	
  		function volver() {
 			window.history.back(-1);
 		}
-		 
-		function cerrar(){
-			window.close();
+		
+		function cargarFecha(){
+			alert('hola');
 		}
+		 
