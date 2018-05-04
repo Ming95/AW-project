7e789dc5d1eafab612baedb2c7cf44f24e9e558e
Ming <36133274+Ming95@users.noreply.github.com>
2018-05-04 22:50:13 +0200

    Events and ideas php
    Each php shows 3 events/ideas that will be displayed at index

---
+++ views/top_events.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+
+<body>
+
+	<div id="box">
+        <h2 id="event_title">Top Eventos</h2>
+        <div id="all_events">
+            <a href="">Ver más eventos &#62&#62</a>
+        </div>    
+        
+		<div class="thumbnail"> 
+            <a href ="https://www.w3schools.com/">
+                <img class ="previewImg" src="images/img_forest.jpg">
+                <p class="description">descripción del evento corto</p>
+            </a>
+            <div id ="social">
+            <a href ="https://twitter.com/?lang=es">
+                <img class="twitter" src= "images/twitter.jpg">
+            </a>
+            </div>
+		</div>
+        <div class="thumbnail">
+            <a href ="https://www.w3schools.com/">
+                <img class ="previewImg" src="images/img_forest.jpg">
+                <p class="description">descripción del evento corto asjhdfasefijwaeghñ asdfghawepghasidgiohaew</p>
+            </a>
+            <a href ="https://twitter.com/?lang=es">
+                <img class="twitter" src= "images/twitter.jpg">
+            </a>
+		</div>
+        <div class="thumbnail">
+            <a href ="https://www.w3schools.com/">
+                <img class ="previewImg" src="images/img_forest.jpg">
+                <p class= "description">descripción del evento corto</p>
+            </a>
+            <a href ="https://twitter.com/?lang=es">
+                <img class="twitter" src= "images/twitter.jpg">
+            </a>
+		</div>
+	</div>
+</body>
+</html>
---
+++ views/top_ideas.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+</head>
+
+<body>
+	<div id="box">
+        <div id="all_ideas">
+            <a href="">Ver más ideas &#62&#62</a>
+        </div>
+		<div class="thumbnail">
+            <a href ="https://www.w3schools.com/">
+                <img class ="previewImg" src="images/img_forest.jpg">
+                <p class= "description">descripción de las foto</p>
+            </a>
+		</div>
+        <div class="thumbnail">
+            <a href ="https://www.w3schools.com/">
+                <img class ="previewImg" src="images/img_forest.jpg">
+                <p class= "description">descripción de las foto</p>
+            </a>
+		</div>
+        <div class="thumbnail">
+            <a href ="https://www.w3schools.com/">
+                <img class ="previewImg" src="images/img_forest.jpg">
+                <p class= "description">descripción de las foto</p>
+            </a>
+		</div>
+	</div>
+</body>
+</html>
