21643d1f46e0adb18f16f24c4b64f1c63bec52fe
Ming <36133274+Ming95@users.noreply.github.com>
2018-05-05 16:43:07 +0200

    Error page update
    Change at error page to show a image and styles

--- errorpage.php
+++ errorpage.php
@@ -2,20 +2,28 @@
 <html>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <head>
-<link rel="stylesheet" type="text/css" href="/css/styleforms.css" />
+<link rel="stylesheet" type="text/css" href="css/errorpage.css" />
 <script type="text/javascript" src="../js/utilidea.js"></script>
 </head>
 <body>
 
 
 <div class="container">
+    
     <div class="row">
-	<?php
-		session_start();
-        echo '<label class="texto2">'.$_SESSION['data_error'].'</label>';
-	?>
+        <div class="image-error">
+            <img class="error" src="images/robot-error.png" alt="Imagen de un robot que muestra cara de no saber lo que falla.">
+        </div>
+        <div class="error-msg">
+            <h1>Oops!</h1>
+            <?php
+                session_start();
+                echo '<label class="texto2">'.$_SESSION['data_error'].'</label>';
+            ?>
+        </div>
+        
     </div>
-     <input class="submit" type="submit" value="Cerrar" onclick="volver()">
+    <input class="submit" type="submit" value="Cerrar" onclick="volver()">
 </div>
 
 </body>
