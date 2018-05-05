41fbcd5061229f4d605592d7b2afd6ffba4bec62
Ming <36133274+Ming95@users.noreply.github.com>
2018-05-05 20:03:51 +0200

    Page header changes.
    Changes at the struture of the header and its style

--- views/layout/head.php
+++ views/layout/head.php
@@ -1,24 +1,30 @@
-<?php
-	echo"<header id="."main-header".">";
-		 if(!isset($_SESSION)) { session_start(); } 
-		//var_dump($_SESSION["logged"]);
-		//var_dump($_SESSION["login"]);
-		//$log = $_SESSION["logged"];//la variable login viene de modelo.
-		echo"<a id="."logo-header"." href="."../index.php".">";
-			echo"<span class="."site-name".">SelfIdea</span>";
-		echo"</a>";
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<head>
+<link rel="stylesheet" type="text/css" href="../css/head.css" />
+    
+<script type="text/javascript" src="../js/utilidea.js"></script>
+</head>
+<body>
+    <?php if(!isset($_SESSION)) { session_start(); } ?>
+    <header id="container">
+        <div class="wrapper">
+            <a class="logo" href="../index.php">SelfIdea</a>
+            
+            <div id="derecha">
+                <?php 
+                    if (isset($_SESSION["logged"]) && ($_SESSION["logged"]===true)) {
+                        echo"<label class="."sesion".">Bienvenido, {$_SESSION['login']} <a href='/views/logout.php'>Cerrar Sesion</a></label>";
 
-		echo"<nav>";
-			echo"<ul class="."user".">";
-				if (isset($_SESSION["logged"]) && ($_SESSION["logged"]===true)) {
-					echo"<li>Bienvenido, {$_SESSION['login']} <a href='/views/logout.php'>Cerrar Sesion</a></li>";
-				}else{
-					//echo"<li><a href="."../views/login.php".">SignUp"."</a></li>";
-					echo "<li><a href='../views/login.php'>Login</a> / <a href='../views/signup.php'>Registro</a></li>";
-				}
-			echo"</ul>";
-		echo"</nav>";
-	echo"</header>";
+                    }else{
+                        //echo"<li><a href="."../views/login.php".">SignUp"."</a></li>";
+                        echo "<label class="."sesion"."><a href='../views/login.php'>Login</a> / <a href='../views/signup.php'>Registro</a></li>";
+                    }
+                ?>
+            </div>
+        </div>
+    </header>
 
-	echo"<hr>";
-?>
+</body>
+    
+</html>
