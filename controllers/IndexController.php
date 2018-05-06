aa624ca26cc54cb176d50e2b15b0a4508d77c891
rosa <rozumaeta@ucm.es>
2018-05-06 15:24:36 +0200

    feat(idea-controllers): Se obtienen eventos para cargar index - IndexController.php

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -1,20 +1,23 @@
  <?php
+ 	include '../models/entidadBase.php';
 	Include '../models/idea.php';
+	Include '../models/evento.php';
+
 	session_start();
-	if(($_GET['numIdeas']==null) or empty($_GET['numIdeas'])) {
+/* 	if(($_GET['numIdeas']==null) or empty($_GET['numIdeas'])) {
 		$_SESSION['data_error']="Ha ocurrido un problema con los datos enviados";
 		header("Location:/errorpage.php");
-	}
-	$numIdeas= htmlspecialchars(trim(strip_tags($_GET["numIdeas"])));
+	} */
+	//$numIdeas=3; //htmlspecialchars(trim(strip_tags($_GET["numIdeas"])));
+	$numIdeas=3;
+	$numEventos=3;
 	try{
-		$idea = new Idea();
 		$topIdeas=obtenerIdeas("popularidad");
-		$idea->closeConnection();
+		$topEventos=obtenerEventos("fecha");
 		$data= array();
 		$data['topIdeas'] = $topIdeas;
+		$data['topEventos'] = $topEventos;
 		$_SESSION['data'] = $data;
-		//header("Location:index.php");
-
 	}catch(Exception $e){
 		error_log("MySQL: Code: ".$e->getCode(). " Desc: " .$e->getMessage() ,0);
 		$_SESSION['data_error']=$e->getMessage();
@@ -22,15 +25,44 @@
 	}
 
 	function obtenerIdeas($orderBy){
-		global $numIdeas, $idea;
+		global $numIdeas;
+		$idea = new Idea();
 		try{
 			$datos=$idea->getNumElemsOrderBy($orderBy,$numIdeas);
+			$idea->closeConnection();
 			return $datos;
 		}catch(Exception $e){
 			error_log("MySQL: Code: ".$e->getCode(). " Desc: " .$e->getMessage() ,0);
 			throw new Exception($e);
 		}
 	}
+	
+	function obtenerEventos($orderBy){
+		global $numEventos, $evento;
+		$evento = new Evento();
+		try{
+			$datos=$evento->getNumElemsOrderBy($orderBy,$numEventos);
+			$evento->closeConnection();
+			return $datos;
+		}catch(Exception $e){
+			error_log("MySQL: Code: ".$e->getCode(). " Desc: " .$e->getMessage() ,0);
+			throw new Exception($e);
+		}
+	}
+	
+/* 	function recuperaIdeas(){
+		
+		$topIdeas=obtenerIdeas("popularidad");
+		
+		return $topIdeas;
+	}
+	
+	function recuperaEventos(){
+		
+		$topEventos=obtenerEventos("fecha");
+		
+		return $topEventos;
+	} */
 
 
 ?>
