cefe70b119bb66927cd6e71a2d296735f65fcc05
rosa <rozumaeta@ucm.es>
2018-05-06 22:18:29 +0200

    fix(idea) Se arreglan vistas

--- views/top_events.php
+++ views/top_events.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,48 +5,7 @@
 </head>
 
 <body>
-    <!--box contendra a los 3 eventos -->
-	<div id="box">
-        <h2 id="event_title">Top Eventos</h2>
-        <div id="all_events">
-            <a href="">Ver más eventos</a>
-        </div>    
-        <!--thumbnail es la caja que contiene la imagen del evento, su descripción, y una imagen del logo de twitter que
-redirige a la página principal de Twitter. La idea es que lleve en el futuro a la pagina de Twitter de SelfIdea.-->
-		<div class="thumbnail">
-            <!--Esta parte se ha hecho con href para que pinche donde se pinche lleve a la pagina del evento. -->
-            <a href ="/controllers/ConsultarEventoController.php?id_evento=1">
-                <img class ="previewImg" src="images/img_forest.jpg">
-                <p class="description">descripción del evento corto</p>
-            </a>
-            <div id ="social">
-            <a href ="https://twitter.com/?lang=es">
-                <img class="twitter" src= "../images/twitter.jpg">
-            </a>
-            </div>
-		</div>
-        <div class="thumbnail">
-            <a href ="/controllers/ConsultarEventoController.php?id_evento=1">
-                <img class ="previewImg" src="../images/img_forest.jpg">
-                <p class="description">descripción del evento corto asjhdfasefijwaeghñ asdfghawepghasidgiohaew</p>
-            </a>
-            <a href ="https://twitter.com/?lang=es">
-                <img class="twitter" src= "../images/twitter.jpg">
-            </a>
-		</div>
-        <div class="thumbnail">
-            <a href ="/controllers/ConsultarEventoController.php?id_evento=1">
-                <img class ="previewImg" src="../images/img_forest.jpg">
-                <p class= "description">descripción del evento corto</p>
-            </a>
-            <a href ="https://twitter.com/?lang=es">
-                <img class="twitter" src= "images/twitter.jpg">
-            </a>
-		</div>
-	</div>
-</body>
-</html>
-=======
+
 <div id="box">
         <h2 id="event_title">Top Eventos</h2>
         <div id="all_events">
@@ -70,4 +28,4 @@ redirige a la página principal de Twitter. La idea es que lleve en el futuro a 
 		?>
     
 	</div>
->>>>>>> ff6ea5c63219951b65d003ca2a8d5cf6b4c0525e
+
