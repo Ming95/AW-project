2a058d7a954d88a8aef5c385a51743c790599234
Jenniza <jezapata@ucm.es>
2018-05-06 04:42:52 +0200

    muestra los eventos de la bbdd(top_events.php)

--- views/top_events.php
+++ views/top_events.php
@@ -1,46 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-</head>
-
-<body>
-
-	<div id="box">
+<div id="box">
         <h2 id="event_title">Top Eventos</h2>
         <div id="all_events">
-            <a href="">Ver más eventos &#62&#62</a>
+            <a href="../views/MasEventos.php">Ver más eventos &#62&#62</a>
         </div>    
         
-		<div class="thumbnail"> 
-            <a href ="/controllers/ConsultarEventoController.php?id_evento=1">
-                <img class ="previewImg" src="images/img_forest.jpg">
-                <p class="description">descripción del evento corto</p>
-            </a>
-            <div id ="social">
-            <a href ="https://twitter.com/?lang=es">
-                <img class="twitter" src= "../images/twitter.jpg">
-            </a>
-            </div>
-		</div>
-        <div class="thumbnail">
-            <a href ="/controllers/ConsultarEventoController.php?id_evento=1">
-                <img class ="previewImg" src="../images/img_forest.jpg">
-                <p class="description">descripción del evento corto asjhdfasefijwaeghñ asdfghawepghasidgiohaew</p>
-            </a>
-            <a href ="https://twitter.com/?lang=es">
-                <img class="twitter" src= "../images/twitter.jpg">
-            </a>
-		</div>
-        <div class="thumbnail">
-            <a href ="/controllers/ConsultarEventoController.php?id_evento=1">
-                <img class ="previewImg" src="../images/img_forest.jpg">
-                <p class= "description">descripción del evento corto</p>
-            </a>
-            <a href ="https://twitter.com/?lang=es">
-                <img class="twitter" src= "images/twitter.jpg">
-            </a>
-		</div>
+		<?php
+			$i = 0;
+			while($i <3){
+				$id = $_SESSION["data1"]["mas_eventos"][$i]["id"];
+				$imagen = $_SESSION['data1']['mas_eventos'][$i]['imagen'];
+				$nombre = $_SESSION['data1']['mas_eventos'][$i]['nombre'];
+				echo '<div class="thumbnail">';
+				echo '<a href="../controllers/ConsultarEventoController.php?id_evento='.$id.'">';
+				echo '<img class ="previewImg" src= "'.$imagen.'">';
+				echo '<p class= "description">'.$nombre.'</p></a></div>';
+				
+				$i++;
+			}
+		?>
+    
 	</div>
-</body>
-</html>
