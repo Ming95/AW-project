73b6c172361982813b449e80842d2a19966f3435
Miguel Arriba <marriba@ucm.es>
2018-05-19 20:13:08 +0200

    Rediseño completo de infoIdea

--- views/infoidea.php
+++ views/infoidea.php
@@ -2,6 +2,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <link rel="stylesheet" type="text/css" href="../css/idea.css" />
+<link rel="stylesheet" type="text/css" href="../css/formulario.css" />
 <script type="text/javascript" src="../js/utilidea.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <?php
@@ -11,27 +12,49 @@
 	$idea->load($_GET['id_idea']);
 ?>
 </head>
-<body>
+<body onload="myfunction1(this)">
+<?php include '../views/layout/head.php';
+		echo '<div id="idea">
+					<div id="cabecera">
+					<h1 id="title">'.$idea->getNombre_Idea().'</h1>
+					<h2 id="subtitle">'.$idea->getCategoria().'<h2></div>
 
-<?php include '../views/layout/head.php';?>
+					<div id="acciones-idea">
+					<img id="main-img" src="'.$idea->getImagen().'" alt="Imagen de la idea">'.
 
-<div class="a">
+					'<div id="lateral">'.
 
-	<div class="b">
-		<h1><?php echo $idea->getNombre_Idea();?></h1>
-		<div class="gallery">
-			<img class="main-img" src="<?php echo $idea->getImagen();?>" alt="Forest">
+					'<p class="numInfo">'.$idea->getRecaudado().' €</p>'.
+					'<p class="textInfo">de la meta de '.$idea->getImporte_solicitado().' €</p>'.
+
+					'<p class="numInfo2">'.$idea->getPopularidad().' €</p>'.
+					'<p class="textInfo">personas han valorado esta idea</p>'.
+
+					'<p class="numInfo2">'.$idea->getDiasFin().'</p>'.
+					'<p class="textInfo">días más</p>'
+
+					;?>
+					<input type="submit" class="boton-formulario" value="Patrocinar" onclick = "location='../views/patrocina.php'"/>
+					<?php
+					if($idea->getEnVenta())
+							echo '<input type="submit" class="boton-formulario2" value="Comprar idea" onclick = "location=\'../views/compraidea.php\'"/>';
+					?>
+					<button class="boton-formulario2"><i class="fa fa-thumbs-up"></i></button>
+				</div><!--lateral-->
+			</div><!--acciones-->
+			<!--		Descripcion			-->
+	<div id="panel">
+			<ul class="lista">
+				<li><a class="active"  id="lista1" href="#Descripcion" onclick="myfunction1(this)" >Descripción</a></li>
+				<li><a class="nactive" id="lista2"  href="#Comentario" onclick="location='../controllers/ComentarioController.php?id_idea=<?php echo $idea->getId_idea();?>&opcion=2'">Comentario</a></li>
+				<li><a class="nactive" id="lista3"  href="#Equipo" onclick="myfunction3(this)">Equipo</a></li>
+			</ul>
+
+		<div id="datos1">
+			<p class="textarea1" name="textarea1" id="textarea1" disabled> <?php echo $idea->getDesc_idea();?></p>
 		</div>
-		<ul class="lista">
-			<li><a class="active"  id="lista1" href="#Descripcion" onclick="myfunction1(this)" >Descripción</a></li>
-			<li><a class="nactive" id="lista2"  href="#Comentario" onclick="location='../controllers/ComentarioController.php?id_idea=<?php echo $idea->getId_idea();?>&opcion=2'">Comentario</a></li>
-			<li><a class="nactive" id="lista3"  href="#Equipo" onclick="myfunction3(this)">Equipo</a></li>
-		</ul>
-		<div class="datos1" id="datos1">
-			<textarea class="textarea1" name="textarea1" id="textarea1" rows="20" cols="82" disabled> <?php echo $idea->getDesc_idea();?>
-			</textarea>
-		</div>
-		<div class="datos2" id="datos2">
+		<!--		Comentarios			-->
+		<div id="datos2">
 			<div class="row">
 				<label class="texto"> Introduzca los comentarios:</label>
 			</div>
@@ -47,34 +70,18 @@
 				</div>
 			</div>
 		</div>
-		<div class="datos3" id="datos3" >
+		<!--		Curriculum			-->
+		<div id="datos3" >
 			<label class="texto">Pulse aquí para descargar el CV del equipo</label>
 			<a href="../CV_equipo.doc" target='_blank' title="Click here to open a Word document"><?php echo $idea->getCv_equipo();?></a>
 		</div>
-	</div>
 
 	<script type="text/javascript">
 		myfunction2('<?php echo json_encode($comentarios)?>');
 	</script>
 
-<div class="c">
-		<span class="label other">Recaudado: <?php echo $idea->getRecaudado();?> € / <?php echo $idea->getImporte_solicitado();?> €</span><br><br><br>
-		<span class="label other">Faltan <?php echo $idea->getDiasFin();?> dias para finalizar</span><br><br><br>
-		<span class="label other">Le gusta a <?php echo $idea->getPopularidad();?> personas.</span><br><br><br>
-		<input type="submit" id= "button" class="button" value="Patrocinar" onclick = "location='../views/patrocina.php'"/>
-		<input type="submit" id= "button" class="button" value="Reportar incidencia" onclick = "location='../views/reportaincidencia.php'"/>
-		<?php
-		if($idea->getEnVenta()){
-				echo '<input type="submit" id= "button" class="button" value="Comprar idea" onclick = "location=';
-				echo "'../views/compraidea.php'";
-				echo '"/>';
-			}
-		?>
-		<br>
-		<button class="icon-transp"><i class="fa fa-thumbs-up"></i></button>
-</div>
-
-
-</div>
+		<input type="submit" class="boton-formulario2" value="Reportar incidencia" onclick = "location='../views/reportaincidencia.php'"/>
+	</div><!--panel-->
+</div><!--idea-->
 <?php include 'layout/foot_page.php';?>
 </body>
