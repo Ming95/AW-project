4ee559b2aa48ecf5610c061f814edddf4cadbf7d
Usuario Local <Usuario Local>
2018-05-25 19:59:45 +0200

    incidencia con bbdd

--- views/reportaincidencia.php
+++ views/reportaincidencia.php
@@ -17,16 +17,17 @@
     <div class="row">
         <label class="texto">Introduzca los comentarios que desea reportar:</label>
     </div>
-    <div class="row">
-        <textarea class="input-text" id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
-
-    </div>
-    <div class="row">
+	<div class="row">
+   <form method="post" action="incidenciaOk.php?id_idea=<?php echo $idea->getId_idea();?>" style="display: inline;">
+				<textarea required class="input-text" name="reporte" placeholder="Escribe un comentario..." style="height:200px"></textarea></br>
+		   	<input type="submit" id= "button"  class="submit" value="Enviar"/>
+	</form>
+    
     <?php
-      echo '<input class="submit" type="submit" value="Enviar" onclick = "location=\'incidenciaOk.php?id_idea='.$idea->getId_idea().'\'"/> ';
-	   
+        
     echo ' <input class="submit" type="button" value="Cancelar" onclick = "location=\'../views/infoidea.php?id_idea='.$idea->getId_idea().'\'"/>';
     ?>
+	</div>
     </div>
 </div>
 
