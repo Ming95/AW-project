522d5160460f948fc028af1803f2f80a85db092e
Miguel Arriba <marriba@ucm.es>
2018-05-28 18:05:02 +0200

    fix(rutas)

--- controllers/like.php
+++ controllers/like.php
@@ -5,6 +5,6 @@ require '../models/rating.php';
 $like = new Rating();
 if($_GET['liked'])$like->dislike($_GET['id'], $_GET['mail']);
 else $like->like($_GET['id'], $_GET['mail']);
-header("Location: ../views/infoidea.php?id_idea=".$_GET['id']."");
+header("Location: ../views/infoIdea.php?id_idea=".$_GET['id']."");
 
  ?>
