ecf569de3b7da74e2e6f9d85edebb6f26e81fde3
Ming <36133274+Ming95@users.noreply.github.com>
2018-06-02 19:33:06 +0200

    View Changes

--- views/profile.php
+++ views/profile.php
@@ -6,9 +6,9 @@
 <head>
   <meta charset="utf-8">
 	<link rel="shortcut icon" href="../images/icon.png" />
-  <link rel="stylesheet" type="text/css" href="../css/formulario.css" />
   <link rel="stylesheet" type="text/css" href="../css/profile.css" />
-
+    <link rel="stylesheet" type="text/css" href="../css/formulario.css" />
+    
   <title>Mi perfil</title>
 </head>
 <body>
@@ -28,7 +28,6 @@ try{
 		$eventos = $user->eventos();
 		$comentario = new Comentario();
 		$comentarios= $comentario->getListaUsuario($user->getIdCorreo());
-
 /*
 		echo $user->getNombre();
 		echo $user->getPassword();
@@ -59,7 +58,7 @@ try{
 			<link rel="stylesheet" type="text/css" href="../css/top_events.css" />
 
 		*/
-
+        
 		/*
 		array de ideas a las que el usuario a aportado
 		print_r($aportaciones);
@@ -108,7 +107,7 @@ try{
 		array con todas las ideas.
 		print_r($allist);
 		*/
-
+        
 		/*
 		array con todos los usuarios.
 		print_r($userlist);
@@ -121,84 +120,123 @@ try{
 }
 ?>
 <h1 class="error">WORK IN PROGRESS</h1>
+    
   <!--Datos de usuario-->
   <div class="profile-settings">
-		<div class="lista-perfil">	
-          <input type="submit" class="boton-formulario" id="modificarPersonalInfo" value="MODIFICAR DATOS PERSONALES" onclick = "location='cambiarPersonalInfo.php'"/>
-		 </div>
-		 <div class="lista-perfil">
-		  <input type="submit" class="boton-formulario" id="modificarPass" value="MODIFICAR CONTRASEÑA" onclick = "location='cambiarPass.php'"/>
-		  </div>
-    <!--form action="cambiarnombre.php" class="formulario">
-        <div class="campos-formulario">
-					<h4>Email:</h4> <input class ="input-box" type="text" name="fname" value="<?php echo "{$_SESSION['mail']}" ?>" disabled><br>
-          <h4>Nombre completo:</h4> <input class ="input-box" type="text" name="fname" value="<?php echo "{$_SESSION['login']}" ?>"><br>
-          <h4>Contraseña:</h4> <input class ="input-box" type="password" name="fname" value="<?php echo "{$_SESSION['login']}" ?>"readonly><br>
-        </div>
-        <div class="submit-formulario">
-          <input type="submit" value="BORRAR CUENTA" class ="boton-formulario2">
-          <input type="submit" value="GUARDAR Y ACTUALIZAR" class ="boton-formulario">
-        </div>
-  </form-->
-
-
-  <!--Mis Ideas-->
-  <div class="lista-perfil">
+      
+      <div class="user_data">
+          <img id="user_img" alt="imagen por defecto de los usuarios" src ="/images/usuario.jpg">
+          
+          <p class="data" >Nombre: <?php echo $user->getNombre(); ?></p>
+          <p class="data" >Correo: <?php echo $user->getIdCorreo(); ?></p>
+      </div>
+      
+      <div class="change">
+          <div class="bottons">	
+            <input type="submit" class="boton-formulario" id="modificarPersonalInfo" value="MODIFICAR DATOS PERSONALES" onclick = "location='cambiarPersonalInfo.php'"/>
+          </div>
+          <div class="bottons">
+            <input type="submit" class="boton-formulario" id="modificarPass" value="MODIFICAR CONTRASEÑA" onclick = "location='cambiarPass.php'"/>
+          </div>
+      
+      </div>
+      
+      
+      <?php
+          if($user->isAdmin()){
+            echo'<div class="change">';
+                echo'<div class="bottons">';	
+                    echo'<input type="submit" class="boton-formulario" value="Lista ideas" onclick = "location.href=\'../views/idealist.php\'"/>';
+                echo'</div>';
+            echo'</div>';
+            }
+        ?>
+      
+      <div class="lista-perfil">
       <h1>Mis Ideas</h1>
-    <div class="fila-perfil">
-      <div class="texto-dato">
-        <h3>Idea genial</h3>
-        <p>100€ / 1000€</p>
-
-      </div>
+          <?php
+            
+            $idealist = $misideas->getList();
+            $top = count($idealist);
+            if($top == 0){
+                echo' <p>Todavia no has creado ninguna idea.</p>';
+            }else{
+                $i =0;
+                while($i < $top){
+                    $nombre = $idealist[$i]['nombre_idea'];
+                    $precio = $idealist[$i]['importe_solicitado'];
+                    $recaudado = $idealist[$i]['recaudado'];
+                    $id = $idealist[$i]['id_idea'];
+                    echo '<a class="links" href="../views/infoIdea.php?id_idea='.$id.'">';
+                    echo '<div class="fila-perfil">';
+                        echo '<div class ="texto-dato">';
+                            echo'<h3>'. $nombre.'</h3>';
+                            echo'<p>'. $recaudado. '€/ '. $precio .'€</p>';
+                        echo'</div>';
+                    echo'</div></a>';
+                    $i++;
+                }
+            }
+          ?>
     </div>
-    <div class="fila-perfil">
-      <div class="texto-dato">
-        <h3>Idea fantástica</h3>
-        <p>800€ / 950€</p>
-      </div>
-    </div>
-  </div>
 
   <!--Ideas a las que he aportado-->
   <div class="lista-perfil">
       <h1>Mis Aportaciones</h1>
-    <div class="fila-perfil">
-      <div class="texto-dato">
-        <h3>Idea genial</h3>
-        <p>100€ / 1000€</p>
-      </div>
-      <div class="boton-fila-perfil">
-        <p>AÑADIR APORTACION</p>
-      </div>
-    </div>
-    <div class="fila-perfil">
-      <div class="texto-dato">
-        <h3>Idea fantástica</h3>
-        <p>800€ / 950€</p>
-      </div>
-      <div class="boton-fila-perfil">
-        <p>AÑADIR APORTACION</p>
-      </div>
-    </div>
+      
+      <?php
+        $top = count($aportaciones);
+        if($top == 0){
+            echo' <p>Todavia no has hecho ninguna aportación.</p>';
+        }else{
+            $i =0;
+            while($i < $top){
+                $nombre = $aportaciones[$i]['nombre_idea'];
+                $precio = $aportaciones[$i]['importe_solicitado'];
+                $id= $aportaciones[$i]['id_idea'];
+                echo '<a class="links" href="../views/infoIdea.php?id_idea='.$id.'">';
+                    echo '<div class="fila-perfil">';
+                        echo '<div class ="texto-dato">';
+                            echo'<h3>'. $nombre.'</h3>';
+                            echo'<p>'. $precio. '€</p>';
+                        echo'</div>';
+                echo'</div></a>';
+                $i++;
+            }
+        }
+            
+        ?>
   </div>
 
   <!--eventos-->
   <div class="lista-perfil">
     <h1>Próximos Eventos</h1>
-    <div class="fila-perfil">
-      <div class="texto-dato">
-        <h3>Data Mining</h3>
-        <p>Madrid 18/05/2018</p>
-      </div>
-    </div>
-    <div class="fila-perfil">
-      <div class="texto-dato">
-        <h3>Web Develop</h3>
-        <p>Barcelona 2/06/2018</p>
-      </div>
-    </div>
+    <?php
+            $top = count($eventos);
+            if($top == 0){
+                echo' <p>Todavia no te has suscrito a ningún evento</p>';
+            }else{
+                $i =0;
+                while($i < $top){
+                    $nombre = $eventos[$i]['nombre'];
+                    $fecha = $eventos[$i]['fecha'];
+                    $incrib = $eventos[$i]['num_usuarios'];
+                    $id = $eventos[$i]['id'];
+                    
+                    echo '<a class="links" href="../views/infoevento.php?id_evento='.$id.'">';
+                        echo '<div class="fila-perfil">';
+                            echo '<div class ="texto-dato">';
+                                echo'<h3>'. $nombre.'</h3>';
+                                echo'<p>'. $fecha. ' |'.$incrib.' de personas inscritas</p>';
+                            echo'</div>';
+                    echo'</div></a>';
+                    $i++;
+                }
+            }
+            
+          ?>
   </div>
 </div>
+      
 <?php include './layout/foot_page.php';?>
 </body>
