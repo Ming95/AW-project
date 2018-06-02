d0da40b204de4ff64bbec09d39ef1835c8ba0c90
Ming <36133274+Ming95@users.noreply.github.com>
2018-06-02 21:29:23 +0200

    Customer Support
    Added customer support pages in the foot page section

---
+++ views/term.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Términos de uso</title>
+    <link rel="shortcut icon" href="../images/icon.png" />
+    <link rel="stylesheet" type="text/css" href="../css/howto.css" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+    
+<body>
+    <?php include '../views/layout/head.php';?>
+    <div class="text">
+        <h1>Término y condición de uso</h1>
+        
+        <h2>1. Introducción</h2>
+        <p>
+            Las presentes condiciones generales de uso de la página web, regulan los términos y condiciones de acceso y uso de container.fdi.ucm.es:20089, propiedad de SelfIdea, con domicilio en C\ San Ingeniero Nº 42 y con Código de Identificación Fiscal número 00700142, en adelante, SelfIdea, que el usuario del Portal deberá de leer y aceptar para usar todos los servicios e información que se facilitan desde el portal. El mero acceso y/o utilización del portal, de todos o parte de sus contenidos y/o servicios significa la plena aceptación de las presentes condiciones generales de uso.
+        </p>
+        
+        <h2>2. Condiciones de uso</h2>
+        <p>
+            Las presentes condiciones generales de uso del portal regulan el acceso y la utilización del portal, incluyendo los contenidos y los servicios puestos a disposición de los usuarios en y/o a través del portal, bien por el portal, bien por sus usuarios, bien por terceros. No obstante, el acceso y la utilización de ciertos contenidos y/o servicios puede encontrarse sometido a determinadas condiciones específicas.
+        </p>
+        
+        <h2>3. Modificaciones</h2>
+        <p>
+            La empresa se reserva la facultad de modificar en cualquier momento las condiciones generales de uso del portal. En todo caso, se recomienda que consulte periódicamente los presentes términos de uso del portal, ya que pueden ser modificados.
+        </p>
+        
+        <h2>5. Responsabilidades del portal</h2>
+        <p>
+            El usuario deberá respetar en todo momento los términos y condiciones establecidos en las presentes condiciones generales de uso del portal. De forma expresa el usuario manifiesta que utilizará el portal de forma diligente y asumiendo cualquier responsabilidad que pudiera derivarse del incumplimiento de las normas.
+
+            Así mismo, el usuario no podrá utilizar el portal para transmitir, almacenar, divulgar promover o distribuir datos o contenidos que sean portadores de virus o cualquier otro código informático, archivos o programas diseñados para interrumpir, destruir o perjudicar el funcionamiento de cualquier programa o equipo informático o de telecomunicaciones.
+        </p>
+        
+        <h2>5. Modificaciones</h2>
+        <p>
+            El usuario conoce y acepta que el portal no otorga ninguna garantía de cualquier naturaleza, ya sea expresa o implícita, sobre los datos, contenidos, información y servicios que se incorporan y ofrecen desde el Portal.
+
+            Exceptuando los casos que la Ley imponga expresamente lo contrario, y exclusivamente con la medida y extensión en que lo imponga, el Portal no garantiza ni asume responsabilidad alguna respecto a los posibles daños y perjuicios causados por el uso y utilización de la información, datos y servicios del Portal.
+
+            En todo caso, el Portal excluye cualquier responsabilidad por los daños y perjuicios que puedan deberse a la información y/o servicios prestados o suministrados por terceros diferentes de la Empresa. Toda responsabilidad será del tercero ya sea proveedor o colaborador.
+        </p>
+        <h2>6. Propiedad intelectual e industrial</h2>
+        <p>
+            Todos los contenidos, marcas, logos, dibujos, documentación, programas informáticos o cualquier otro elemento susceptible de protección por la legislación de propiedad intelectual o industrial, que sean accesibles en el portal corresponden exclusivamente a la empresa o a sus legítimos titulares y quedan expresamente reservados todos los derechos sobre los mismos. Queda expresamente prohibida la creación de enlaces de hipertexto (links) a cualquier elemento integrante de las páginas web del Portal sin la autorización de la empresa, siempre que no sean a una página web del Portal que no requiera identificación o autenticación para su acceso, o el mismo esté restringido.
+
+            En cualquier caso, el portal se reserva todos los derechos sobre los contenidos, información datos y servicios que ostente sobre los mismos. El portal no concede ninguna licencia o autorización de uso al usuario sobre sus contenidos, datos o servicios, distinta de la que expresamente se detalle en las presentes condiciones generales de uso del portal.
+        </p>
+        
+        <h2>7. Legislación aplicable, jurisdicción competente y notificaciones</h2>
+        <p>
+            Las presentes condiciones se rigen y se interpretan de acuerdo con las Leyes de España. Para cualquier reclamación serán competentes los juzgados y tribunales de (indicar la ciudad). Todas las notificaciones, requerimientos, peticiones y otras comunicaciones que el Usuario desee efectuar a la Empresa titular del Portal deberán realizarse por escrito y se entenderá que han sido correctamente realizadas cuando hayan sido recibidas en la siguiente dirección (indicar dirección de correo en la que se desean recibir las notificaciones).
+        </p>
+        
+    </div>
+    
+    
+    
+    <?php include '../views/layout/foot_page.php';?>
+</body>
+    
+</html>
