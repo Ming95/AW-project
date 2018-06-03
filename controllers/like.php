f5cf43d6666b4eba4dcad2829a093ceb45b6ac46
rosa <rozumaeta@ucm.es>
2018-06-03 21:27:58 +0200

    feat(idea-controllers): se modifica redirección para incluir en js

--- controllers/like.php
+++ controllers/like.php
@@ -3,8 +3,12 @@
 require '../models/rating.php';
 
 $like = new Rating();
-if($_GET['liked'])$like->dislike($_GET['id'], $_GET['mail']);
-else $like->like($_GET['id'], $_GET['mail']);
-header("Location: ../views/infoIdea.php?id_idea=".$_GET['id']."");
+if($_GET['liked']){
+	$like->dislike($_GET['id'], $_GET['mail']);
+	return "false";
+}else {
+	$like->like($_GET['id'], $_GET['mail']);
+	return "true";
+}
 
  ?>
