64e38c7554ae5cdbc3844cf737f98d81ba9678ab
rosa <rozumaeta@ucm.es>
2018-06-03 21:24:39 +0200

    feat(idea-js): Se actualiza librería jquery

--- js/utilidea.js
+++ js/utilidea.js
@@ -21,6 +21,8 @@
 			tecla_final = String.fromCharCode(tecla);
 			return patron.test(tecla_final);
 		}
+		
+
 		function myfunction1(e){
 			var elemento1 = document.getElementById("datos1");
 			elemento1.style.display = 'block';
@@ -49,32 +51,6 @@
 		function leerComentario(){
 			var texto = document.getElementById("subject").value;
 			return texto;
-		}
-
-		function myfunction2(coment){
-			var elemento1 = document.getElementById("datos1");
-			elemento1.style.display = 'none';
-			var elemento2 = document.getElementById("datos2");
-			elemento2.style.display = 'block';
-			var elemento3 = document.getElementById("datos3");
-			elemento3.style.display = 'none';
-			document.getElementById("lista2").className = 'active';
-			document.getElementById("lista1").className = 'nactive';
-			document.getElementById("lista3").className = 'nactive';
-		}
-
-
-		function myfunction3(e){
-			var elemento1 = document.getElementById("datos1");
-			elemento1.style.display = 'none';
-			var elemento2 = document.getElementById("datos2");
-			elemento2.style.display = 'none';
-			var elemento3 = document.getElementById("datos3");
-			elemento3.style.display = 'block';
-
-			document.getElementById("lista2").className = 'nactive';
-			document.getElementById("lista1").className = 'nactive';
-			document.getElementById("lista3").className = 'active';
 		}
 
 		function visible(elemento,esVisible) {
@@ -113,6 +89,101 @@
 		  for (i = 0; i < dots.length; i++) {
 			  dots[i].className = dots[i].className.replace(" active", "");
 		  }
+		  if(i>=1){
 		  slides[slideIndex-1].style.display = "block";
 		  dots[slideIndex-1].className += " active";
+		  }
 		}
+		
+		function myfunction1(e){
+			var elemento1 = document.getElementById("datos1");
+			elemento1.style.display = 'block';
+			var elemento2 = document.getElementById("datos2");
+			elemento2.style.display = 'none';
+
+			var elemento3 = document.getElementById("datos3");
+			elemento3.style.display = 'none';
+
+			document.getElementById("lista2").className = 'nactive';
+			document.getElementById("lista1").className = 'active';
+			document.getElementById("lista3").className = 'nactive';
+			
+			
+		}
+		
+		$(document).on('ready',function(){
+		
+		$("#lista1").on( "click", function() {
+			$("#datos1").css("display", "block");
+			$("#datos2").css("display", "none");
+			$("#datos3").css("display", "none");
+			$("#lista1").removeClass('active');
+			$("#lista2").removeClass('active');
+			$("#lista3").removeClass('active');
+			$("#lista1").addClass('active');
+			$("#lista2").addClass('nactive');
+			$("#lista3").addClass('nactive');
+		 });
+		$("#lista2").on( "click", function() {
+			$("#datos1").css("display", "none");
+			$("#datos2").css("display", "block");
+			$("#datos3").css("display", "none");
+			$("#lista1").addClass('nactive');
+			$("#lista2").addClass('active');
+			$("#lista3").addClass('nactive');
+			$("#lista1").removeClass('active');
+			$("#lista2").removeClass('nactive');
+			$("#lista3").removeClass('active');
+		 });
+		$("#lista3").on( "click", function() {
+			$("#datos1").css("display", "none");
+			$("#datos2").css("display", "none");
+			$("#datos3").css("display", "block");
+			$("#lista1").addClass('nactive');
+			$("#lista2").addClass('nactive');
+			$("#lista3").addClass('active');
+			$("#lista1").removeClass('active');
+			$("#lista2").removeClass('active');
+			$("#lista3").removeClass('nactive');
+		 });
+	});
+	
+		$(document).on('ready',function(){
+		$('#like').click(function(){
+			var	id= $("#id-idea").val();
+ 			var	mail= $("#id-mail").val();
+			var	liked= $("#id-liked").val(); 
+			var dataString = 'id=' + id + '&mail=' + mail + '&liked=' + liked;
+        $.ajax({
+                data:  dataString, //datos que se envian a traves de ajax
+                url:   '../controllers/like.php', //archivo que recibe la peticion
+                type:  'GET', //método de envio
+                beforeSend: function () {
+                        $("#resultado").html("Procesando, espere por favor...");
+                },
+                success:  function (result) { //una vez que el archivo recibe el request lo procesa y lo devuelve	
+					window.location = '../views/infoIdea.php?id_idea='+id;
+			   }
+			 });
+		  });
+		});
+
+		$(document).on('ready',function(){
+		$('#boton').click(function(){
+			var	id= $("#id-evento").val();
+ 			var	mail= $("#id-mail").val();
+			var	sub= $("#id-sub").val(); 
+			var dataString = 'id=' + id + '&mail=' + mail + '&sub=' + sub;
+        $.ajax({
+                data:  dataString, //datos que se envian a traves de ajax
+                url:   '../controllers/suscribe.php', //archivo que recibe la peticion
+                type:  'GET', //método de envio
+                beforeSend: function () {
+                        $("#resultado").html("Procesando, espere por favor...");
+                },
+                success:  function (result) { //una vez que el archivo recibe el request lo procesa y lo devuelve	
+					window.location = '../views/infoevento.php?id_evento='+id;
+			   }
+			 });
+		  });
+		});
