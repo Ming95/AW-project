812f23a511d424e6b1d7491b6598ad1987923eb0
rosa <rozumaeta@ucm.es>
2018-06-03 21:28:15 +0200

    feat(idea-controllers): se modifica redirección para incluir en js

--- controllers/suscribe.php
+++ controllers/suscribe.php
@@ -5,6 +5,6 @@ require '../models/subscriptions.php';
 $subs = new Subscription();
 if($_GET['sub']) $subs->unSub($_GET['id'], $_GET['mail']);
 else $subs->sub($_GET['id'], $_GET['mail']);
-header("Location: ../views/infoevento.php?id_evento=".$_GET['id']."");
+
 
  ?>
